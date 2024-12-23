59dd06944f7304efd2e9fcc96abf4a8e52bda09b
JacobLinCool <jacoblincool@gmail.com>
2024-12-23 14:39:43 +0000

    Only enable wav (pcm) to mp3

--- build/ffmpeg.sh
+++ build/ffmpeg.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 CONF_FLAGS=(
   --target-os=none              # disable target specific configs
   --arch=x86_32                 # use x86_32 arch
+  --disable-everything                # Disable all features by default
   --enable-cross-compile        # use cross compile configs
   --disable-asm                 # disable asm
   --disable-stripping           # disable stripping as it won't work
@@ -13,6 +14,16 @@ CONF_FLAGS=(
   --disable-debug               # disable debug mode
   --disable-runtime-cpudetect   # disable cpu detection
   --disable-autodetect          # disable env auto detect
+  --enable-demuxer=wav                # Enable WAV demuxer
+  --enable-muxer=mp3                  # Enable MP3 muxer
+  --enable-parser=mp3                 # Enable MP3 parser
+  --enable-encoder=libmp3lame         # Enable MP3 encoder
+  --enable-decoder=pcm_s16le          # Enable PCM decoder (WAV)
+  --enable-protocol=file
+  --enable-filter=anull,aformat,aresample,pan
+  --disable-avdevice                  # Disable device support
+  --disable-swscale                   # Disable video scaling
+  --disable-network                   # Disable network support
 
   # assign toolchains and extra flags
   --nm=emnm
